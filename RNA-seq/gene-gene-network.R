6560165152851dc2c154a9739b2b7b00edc66df1
taolincj <98137881+taolincj@users.noreply.github.com>
2024-11-28 14:00:27 +0800

    Update gene-gene-network.R

--- RNA-seq/gene-gene-network.R
+++ RNA-seq/gene-gene-network.R
@@ -12,23 +12,24 @@ gtf = gtf %>% filter(type=="gene") %>% select(gene_name, gene_id)
 gtf1 = gtf; names(gtf1) = c("deg1", "deg")
 gtf2 = gtf; names(gtf2) = c("ase1", "ase")
 
-### gene-gene network. expression(DEG) = expression(POE) + sex 
+### gene-gene network. expression(DEG) = expression(POE) + sex + breeding_type
 ggi_e27 = function(zz1, fdr1){
   e1 = fread("gene_tpms_e27.tsv") #所有组织的表达量
   d1 = e1 %>% select(Gene_ID, ends_with(zz1)) %>% 
     column_to_rownames("Gene_ID") %>%
     t() %>% as.data.frame() #某一组织的表达量
   deg = fread(paste0(zz1, "_DEG_notwasp_sig.txt")) #DEG
-  ase = fread(paste0("/data_group/xiehaibing/xiehaibing2/f1/rna/pysam/expression/deg/", 
-                     zz1, "_poe_read10_sig.txt"))
+  ase = fread(paste0("/data_group/xiehaibing/xiehaibing2/f1/rna/pysam/expression/deg/", zz1, "_poe_read10_sig.txt"))
   g1 = deg$geneID
-  g2 = ase$geneID
+  g2 = ase$gene_id
   
-  if (zz1=="jr"){d1$sex = c(1,1,2,2,1,2,1,1,2,1,2,1,2,1,1,2)}
-  if (zz1!="jr"){d1$sex = c(2,2,2,2,2,2,1,2,2,2,2,1,2,1,1,1,1,1,2,1)}
-                            
+  if (zz1=="jr"){d1$sex = c(1,1,2,2,1,2,1,1,2,1,2,1,2,1,1,2);
+  d1$type = c(rep(2,8), rep(1,7), 2)}
+  if (zz1!="jr"){d1$sex = c(2,2,2,2,2,2,1,2,2,2,2,1,2,1,1,1,1,1,2,1);
+  d1$type = c(rep(1,9), rep(2, 10), 1)}
+  
   gg = function(deg, ase){
-    fit = lm(d1[[deg]] ~ d1[[ase]] + d1$sex)
+    fit = lm(d1[[deg]] ~ d1[[ase]] + d1$sex + d1$type)
     co = summary(fit)$coefficients %>% as.data.frame()
     x = c(deg=deg, ase=ase,co["d1[[ase]]", 1:4]) %>% as.data.frame()
     return(x)
@@ -47,6 +48,7 @@ ggi_e27 = function(zz1, fdr1){
     left_join(gtf2, by="ase") %>% arrange(p)
   return(x)
 }
+
 x1 = ggi_e27("rib", fdr1=0.05); fwrite(x1, "rib.ggi.txt", sep = "\t")
 
 
@@ -55,6 +57,7 @@ ggi_d90 = function(zz1, fdr1){
   d1 = e1 %>% select(Gene_ID, ends_with(zz1)) %>% 
     column_to_rownames("Gene_ID") %>%
     t() %>% as.data.frame() #某一组织的表达量
+  d1$type = rep(c(2,1),each=6)
   deg = fread(paste0("/data_group/xiehaibing/xiehaibing2/f1/d90/rna/DIR_STARpass2expression/pass2notwasp/", 
                      zz1, ".pass2.notwasp.deg.txt")) #DEG
   ase = fread(paste0("/data_group/xiehaibing/xiehaibing2/f1/d90/rna/pysam/expression/deg/", 
@@ -63,7 +66,7 @@ ggi_d90 = function(zz1, fdr1){
   g2 = ase$geneID
   
   gg = function(deg, ase){
-    fit = lm(d1[[deg]] ~ d1[[ase]])
+    fit = lm(d1[[deg]] ~ d1[[ase]] + d1$type)
     co = summary(fit)$coefficients %>% as.data.frame()
     x = c(deg=deg, ase=ase,co["d1[[ase]]", 1:4]) %>% as.data.frame()
     return(x)
